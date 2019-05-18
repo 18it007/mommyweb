989e2dd239a74eaf8dc9df9b51b8f38549b7a1d1
Nguyễn Anh Dũng - 18IT1 <44762416+18it007@users.noreply.github.com>
2019-05-18 16:32:44 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,12 +2,6 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const path = require('path');
-// Database
-const db = require('./config/database');
-// Test DB
-db.authenticate()
-  .then(() => console.log('Database connected...'))
-  .catch(err => console.log('Error: ' + err))
 
 const app = express();
 
